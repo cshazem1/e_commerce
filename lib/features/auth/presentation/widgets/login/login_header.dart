49baf4ffe077790e80c6547036d303cd40dae7f2
Khaled Elsaeed <khaledmokhaled123@gmail.com>
2024-09-13 02:17:23 +0300

    added folders

---
+++ lib/features/auth/presentation/widgets/login/login_header.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class LoginHeader extends StatelessWidget {
+  const LoginHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
